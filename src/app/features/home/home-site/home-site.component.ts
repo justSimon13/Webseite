2fc71e6bd43d2d675ecb3923803d27204440dcbe
Simon Fischer <entwicklung@simonfischer.dev>
2025-05-01 01:28:51 +0200

    add Meta Text

--- src/app/features/home/home-site/home-site.component.ts
+++ src/app/features/home/home-site/home-site.component.ts
@@ -1,7 +1,8 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 
 import { CalendlyService } from '../../../core/services/calendly/calendly.service';
 import { ScrollAnimationService } from '../../../core/services/scroll-animation/scroll-animation.service';
+import { SeoService } from '../../../core/services/seo/seo.service';
 import { HOME_IMPORTS } from '../home-shared';
 
 @Component({
@@ -9,11 +10,19 @@ import { HOME_IMPORTS } from '../home-shared';
   imports: [HOME_IMPORTS],
   templateUrl: './home-site.component.html',
 })
-export class HomeSiteComponent implements AfterViewInit {
+export class HomeSiteComponent implements AfterViewInit, OnInit {
   constructor(
+    private seoService: SeoService,
     private scrollAnimationService: ScrollAnimationService,
     private calendlyService: CalendlyService
   ) {}
+
+  ngOnInit(): void {
+    this.seoService.updateMeta(
+      'Webentwicklung für Unternehmen – Simon Fischer | Webseiten, Shops & Apps',
+      'Individuelle Webseiten, Onlineshops & Software – für Unternehmen, die digital durchstarten wollen. Modern, effizient & verständlich umgesetzt.'
+    );
+  }
 
   ngAfterViewInit(): void {
     setTimeout(() => {
